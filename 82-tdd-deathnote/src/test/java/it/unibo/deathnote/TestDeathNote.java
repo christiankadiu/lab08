79c53512e52244dfa00865bb0868c9af82e69410
Your Real Name <kadiuchristian8@gmail.com>
2024-11-12 18:01:24 -0800

    creato il metodo SetUp in TestDeathNote.java

--- 82-tdd-deathnote/src/test/java/it/unibo/deathnote/TestDeathNote.java
+++ 82-tdd-deathnote/src/test/java/it/unibo/deathnote/TestDeathNote.java
@@ -1,5 +1,17 @@
 package it.unibo.deathnote;
 
+import it.unibo.deathnote.impl.DeathNoteImpl;
+
+import org.junit.jupiter.api.BeforeEach;
+
+import it.unibo.deathnote.api.DeathNote;
+
 class TestDeathNote {
 
+    DeathNote deathNode;
+
+    @BeforeEach
+    public void SetUp() {
+        deathNode = new DeathNoteImpl();
+    }
 }
